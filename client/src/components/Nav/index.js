82f34a5eb87359a3f9e394f514a7bc96beeb4a3a
Maria McGovern <mtgovern@me.com>
2020-06-30 22:00:15 -0400

    nav bar and pages

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav className="navbar navbar-expand-lg">
+      <a className="navbar-brand" href="/">
+        Google Books
+      </a>
+      <a className="navbar-brand" href="/search">
+        Search
+      </a>
+      <a className="navbar-brand" href="/saved">
+        Saved
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
